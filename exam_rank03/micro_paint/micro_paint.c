6db2dafba97b33a4d5d8b6ccfec3b888844113b6
Curtis <curtman.73@gmail.com>
2022-01-11 00:09:55 +0100

    micro_paint en cours

--- exam_rank03/micro_paint/micro_paint.c
+++ exam_rank03/micro_paint/micro_paint.c
@@ -6,7 +6,7 @@
 /*   By: cdapurif <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 17:45:46 by cdapurif          #+#    #+#             */
-/*   Updated: 2022/01/10 19:17:32 by cdapurif         ###   ########.fr       */
+/*   Updated: 2022/01/11 00:06:55 by cdapurif         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@
 #include <string.h>
 #include <math.h>
 
-typedef struct s_info
+typedef struct	s_info
 {
-	int	width;
-	int	height;
+	int		width;
+	int		height;
 	char	background;
-}		t_info;
+}				t_info;
 
-typedef struct s_rect
+typedef struct	s_rect
 {
-	char	full;
+	char	fill;
 	float	x;
 	float	y;
 	float	width;
 	float	height;
 	char	c;
-}		t_rect;
+}				t_rect;
 
 void	ft_putstr(char *str)
 {
@@ -43,44 +43,95 @@ void	ft_putstr(char *str)
 	write(1, str, i);
 }
 
-char	*create_map()
+char	*create_map(t_info *info)
+{
+	char	*str;
+	int		i;
+	int		size;
+
+	size = info->width * info->height;
+	str = malloc(sizeof(char) * size + 1);
+	if (!str)
+		return (NULL);
+	str[size] = '\0';
+	i = -1;
+	while (++i < size)
+		str[i] = info->background;
+	return (str);
+}
+
+int	ret_error(FILE *file, int close)
+{
+	ft_putstr("Error: Operation file corrupted\n");
+	if (close)
+		fclose(file);
+	return (1);
+}
+
+void	display_map(t_info *info, char *map)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (++i < info->height)
+	{
+		write(1, map + (i * info->width), info->width);
+		write(1, "\n", 1);
+	}
+}
+
+int	read_lines(t_info *info, FILE *file, char *map)
+{
+	t_rect r;
+	int	ret;
+
+	while ((ret = fscanf(file, "%c %f %f %f %f %c\n", &r.fill, &r.x, &r.y, \
+		&r.width, &r.height, &r.c)) == 6)
+	{
+		printf("line\n");
+	}
+	if (ret != 6 && ret != -1)
+		return (1);
+	return (0);
+}
 
 int	micro_paint(char *path)
 {
-	int	ret;
+	int		ret;
 	FILE	*file;
+	t_info	info;
+	char	*map;
 
 	file = fopen(path, "r");
 	if (!file)
+		return (ret_error(file, 0));
+	if ((ret = fscanf(file, "%d %d %c\n", \
+		&info.width, &info.height, &info.background)) != 3)
+		return (ret_error(file, 1));
+	if (info.width <= 0 || info.width > 300 || info.height <= 0 || \
+		info.height > 300)
+		return (ret_error(file, 1));
+	map = create_map(&info);
+	if (!map)
+		return (ret_error(file, 1));
+	if (read_lines(&info, file, map)) //ici
 	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return (1);
+		free(map);
+		return (ret_error(file, 1));
 	}
-	if ((ret = fscanf(file, "%d %d %c\n", &s->widht, &s->height, &s->background)) != 3)
-	{
-		ft_putstr("Error: Operation file corrupted\n");
-		return (1);
-	}
-	map = create_map;
-	/*while ((ret = fscanf(file, "", &, )) == 6)
-	{
-
-	}*/
-
+	display_map(&info, map);
+	fclose(file);
+	free(map);
 	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	int	ret;
-
-	ret = 0;
-	if (ac == 2)
-		ret = micro_paint(av + 1);
-	else
+	if (ac != 2)
 	{
 		ft_putstr("Error: argument\n");
 		return (1);
 	}
-	return (ret);
+	return (micro_paint(av[1]));
 }
